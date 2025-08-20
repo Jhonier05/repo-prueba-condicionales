af9d30599795c73be77a63ac4bf2135a3267af68
Jhonier Javith Hernández <hernandezjhonier12@gmail.com>
2025-08-20 13:08:51 -0500

    Agrego contenido real a los archivos de Java y Python

--- Java/CondicionalesJava.java
+++ Java/CondicionalesJava.java
@@ -1,1 +1,37 @@
-﻿// Codigo Java
+﻿package actividadc.ondicionales.java;
+
+import java.util.Scanner;
+
+public class ActividadcOndicionalesJava {
+
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("ingrese su nombre : ");
+        String nombre = leer.nextLine();
+        
+        System.out.println("ingrese su apellido : ");
+        String apellido = leer.nextLine();
+        
+        System.out.println("ingrese su edad : ");
+        int edad = leer.nextInt();
+        
+        leer.nextLine();
+        System.out.print("Ingrese su sexo (masculino/femenino): ");
+        String sexo = leer.nextLine();
+         
+        if (edad >= 18){
+            System.out.println(nombre+ " "  + apellido +  " es mayor de edad ");
+        }else{
+            System.out.println(nombre+ " "  + apellido +  " es menor de edad ");
+        }
+         
+        if (sexo.equalsIgnoreCase("masculino")){
+            System.out.println("es un hombre");
+        } else{
+            System.out.println("es una mujer");
+        }
+    }
+}
+
--- Python/condicionales.py
+++ Python/condicionales.py
@@ -1,1 +1,14 @@
-﻿# Codigo Python
+﻿nombre = input("ingrese su nombre: ")
+apellido = input("ingrese su apellido: ")
+edad = int(input("ingrese su edad: "))
+sexo = input("ingrese su sexo : femenino/masculino: ")
+
+if edad >= 18:
+    print(f"{nombre} {apellido} es mayor de edad")
+else:
+    print(f"{nombre} {apellido} es menor de edad")
+
+if sexo.lower() == "masculino":
+    print("es un hombre")
+else:
+    print("es una mujer")
